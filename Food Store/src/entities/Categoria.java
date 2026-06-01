6df2ef61a1e0f7708a70ab62f4a341297b528278
tiago3106 <tiagogd3101@gmail.com>
2026-06-01 19:18:14 -0300

    Actualizacion Categoria
    Se agregaron los atributos y getter y setter simples(get y set)

--- Food Store/src/entities/Categoria.java
+++ Food Store/src/entities/Categoria.java
@@ -1,7 +1,48 @@
 package entities;
-
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 // Definicion de TODO: (TODO = to do (hacer))
 
 public class Categoria extends Base{
     //TODO: escribir los atributos de la clase, usar super en el constructor para pasarle los parametros a base y NO usar smart setters
+    private String nombre;
+    private String descripcion;
+
+    //Relacion 1:N con Producto
+    private List<Producto> productos = new ArrayList<>();
+
+    //Constructor
+    public Categoria(){
+        super();
+    }
+    public Categoria(Long id, boolean eliminado, LocalDateTime createAT, String nombre, String descripcion){
+        super(id, eliminado, createAT);
+
+    }
+
+    //getter and setter
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public List<Producto> getProductos() {
+        return productos;
+    }
+
+    public void setProductos(List<Producto> productos) {
+        this.productos = productos;
+    }
 }
